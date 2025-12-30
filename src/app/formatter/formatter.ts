518cf021b97393cf83ca05ddf6622f6c4d8fc5cb
Jeffrey Jeff Cross <jpcross@worleyco.com>
2025-12-29 22:28:56 -0600

    Material Design added

--- src/app/formatter/formatter.ts
+++ src/app/formatter/formatter.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import {
+    FormArray,
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 interface Damage {
-    index: number;
+    id: string;
     amount: number;
     sides: number;
     bonus: number;
@@ -9,53 +19,77 @@ interface Damage {
 }
 @Component({
     selector: 'app-formatter',
-    imports: [FormsModule],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatIconModule,
+        MatSelectModule,
+        MatButtonModule,
+    ],
     templateUrl: './formatter.html',
     styleUrl: './formatter.scss',
 })
-export class Formatter {
-    attackName = 'Rend';
-    attackType = ['Melee'];
-    toHit = 9;
-    reach = 5;
-    rangeReach1 = 20;
-    rangeReach2 = 60;
-    targets = 'one target';
-    damages: Damage[] = [
-        {
-            index: 1,
-            amount: 1,
-            sides: 6,
-            bonus: 5,
-            type: 'piercing',
-        },
-    ];
-
-    addAttack() {
-        this.damages.push({
-            index: this.damages.length + 1,
-            amount: 1,
-            sides: 6,
-            bonus: 0,
-            type: 'poison',
-        });
-    }
-    get attackTypeText(): string {
-        return this.attackType.join(' or ');
-    }
+export class Formatter implements OnInit {
+    attackForm: FormGroup;
 
     get reachText(): string {
         let returnText = '';
-        if (this.attackType.includes('Melee')) {
-            returnText += `reach ${this.reach} ft.`;
-        }
-        if (this.attackType.length > 1) {
-            returnText += ' or ';
+        if (this.attackType === 'Melee') {
+            returnText += `reach ${this.attackForm.get('reach')?.value} ft.`;
         }
         if (this.attackType.includes('Ranged')) {
-            returnText += `range ${this.rangeReach1} ft./${this.rangeReach2} ft.`;
+            returnText += `range ${this.attackForm.get('range')?.value} ft./${this.attackForm.get('rangeDisadvantage')?.value} ft.`;
         }
         return returnText;
+    }
+
+    get damages(): FormArray {
+        return this.attackForm.get('damages') as FormArray;
+    }
+
+    get attackName(): string {
+        return this.attackForm.get('name')?.value;
+    }
+
+    get attackType(): string {
+        return this.attackForm.get('type')?.value;
+    }
+
+    constructor(private fb: FormBuilder) {
+        this.attackForm = this.fb.group({
+            name: 'Rend',
+            type: 'Melee',
+            toHit: 9,
+            reach: 5,
+            range: 20,
+            rangeDisadvantage: 60,
+            targets: 'one target',
+            damages: this.fb.array([
+                this.fb.group({
+                    id: crypto.randomUUID(),
+                    amount: 1,
+                    sides: 6,
+                    type: 'piercing',
+                    bonus: 5,
+                }),
+            ]),
+        });
+    }
+
+    ngOnInit(): void {}
+
+    addAttack() {
+        const index = this.damages.length;
+        this.damages.push(
+            this.fb.group({
+                id: crypto.randomUUID(),
+                amount: 1,
+                sides: 6,
+                bonus: 0,
+                type: 'poison',
+            }),
+        );
     }
 
     getDamageAverage(amount: number, die: number, bonus = 0): number {
@@ -66,11 +100,24 @@ export class Formatter {
         return !bonus ? '' : ` + ${bonus}`;
     }
 
-    getDamageMessage(item: Damage): string {
-        return (
-            this.getDamageAverage(item.amount, item.sides, item.bonus) +
-            ` [rollable](${item.amount}d${item.sides}${this.getBonusText(item.bonus)}); {"diceNotation":"${item.amount}d${item.sides}${this.getBonusText(item.bonus)}","rollType":"damage","rollAction":"${this.attackName}","rollDamageType":"${item.type}"}[/rollable] ${item.type} damage` +
-            (this.damages.length > item.index ? ' plus ' : '')
-        );
+    get damageMessages(): string {
+        const messages = this.damages.controls.map((item) => {
+            const [amount, sides, bonus, type] = [
+                item.get('amount')?.value,
+                item.get('sides')?.value,
+                item.get('sides')?.value,
+                item.get('type')?.value,
+            ];
+
+            return (
+                this.getDamageAverage(amount, sides, bonus) +
+                ` [rollable](${amount}d${sides}${this.getBonusText(bonus)}); {"diceNotation":"${amount}d${sides}${this.getBonusText(bonus)}","rollType":"damage","rollAction":"${this.attackName}","rollDamageType":"${type}"}[/rollable] ${type} damage`
+            );
+        });
+        return messages.join(' plus ');
+    }
+
+    clear(index: number): void {
+        this.damages.removeAt(index);
     }
 }
